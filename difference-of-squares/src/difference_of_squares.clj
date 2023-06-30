782371d1d954f1955e03c3603ff9d62ee11e6e64
hw <hussainweb@gmail.com>
2023-06-29 21:47:40 -0400

    solve difference-of-squares

--- difference-of-squares/src/difference_of_squares.clj
+++ difference-of-squares/src/difference_of_squares.clj
@@ -1,13 +1,20 @@
 (ns difference-of-squares)
 
-(defn difference [] ;; <- arglist goes here
-  ;; your code goes here
+(defn sum-of-squares [number]
+  (cond (> number 1) (+ (* number number) (sum-of-squares (- number 1)))
+        :else 1
+  )
 )
 
-(defn sum-of-squares [] ;; <- arglist goes here
-  ;; your code goes here
+(defn summation [number]
+  (int (/ (* number (+ number 1)) 2))
 )
 
-(defn square-of-sum [] ;; <- arglist goes here
-  ;; your code goes here
+(defn square-of-sum [number]
+  (def sq (summation number))
+  (* sq sq)
+)
+
+(defn difference [number]
+  (- (square-of-sum number) (sum-of-squares number))
 )
