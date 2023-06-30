01351812df5f6ad7f86b9c693a9dca34c1febd95
hw <hussainweb@gmail.com>
2023-06-30 01:20:05 -0400

    solve elyses-destructured-enchantments

--- elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
+++ elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
@@ -3,22 +3,34 @@
 (defn first-card
   "Returns the first card from deck."
   [deck]
+  (let [[first] deck]
+    first
+  )
 )
 
 (defn second-card
   "Returns the second card from deck."
   [deck]
+  (let [[_ second] deck]
+    second
+  )
 )
 
 (defn swap-top-two-cards
   "Returns the deck with first two items reversed."
   [deck]
+  (let [[first second & rest] deck]
+    (into (vector second first) rest)
+  )
 )
 
 (defn discard-top-card
   "Returns a sequence containing the first card and
    a sequence of the remaining cards in the deck."
   [deck]
+  (let [[first & rest] deck]
+    (vector first rest)
+  )
 )
 
 (def face-cards
@@ -27,4 +39,10 @@
 (defn insert-face-cards
   "Returns the deck with face cards between its head and tail."
   [deck]
+  (let [[first & rest] deck]
+    (if (nil? first)
+      face-cards
+      (into (into (vector first) face-cards) rest)
+    )
+  )
 )
