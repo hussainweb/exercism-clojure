3d2889e0867b84edcde67ad303f2f792428699c2
hw <hussainweb@gmail.com>
2023-06-29 22:09:05 -0400

    solve tracks-on-tracks-on-tracks

--- tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
+++ tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
@@ -3,26 +3,31 @@
 (defn new-list
   "Creates an empty list of languages to practice."
   []
+  (list)
   )
 
 (defn add-language
   "Adds a language to the list."
   [lang-list lang]
+  (list* lang lang-list)
   )
 
 (defn first-language
   "Returns the first language on the list."
   [lang-list]
+  (first lang-list)
   )
 
 (defn remove-language
   "Removes the first language added to the list."
   [lang-list]
+  (rest lang-list)
   )
 
 (defn count-languages
   "Returns the total number of languages on the list."
   [lang-list]
+  (count lang-list)
   )
 
 (defn learning-list
@@ -30,4 +35,11 @@
   Java and JavaScript, then finally returns a count of the total number
   of languages."
   []
+  (def ll (list))
+  (def ll (list* "Clojure" ll))
+  (def ll (list* "Lisp" ll))
+  (def ll (rest ll))
+  (def ll (list* "Java" ll))
+  (def ll (list* "JavaScript" ll))
+  (count ll)
   )
