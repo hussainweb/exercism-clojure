09509b764a974765d0791b83b9e28824d06cd127
hw <hussainweb@gmail.com>
2023-06-29 23:55:28 -0400

    solve robot-name

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -1,13 +1,42 @@
 (ns robot-name)
 
-(defn robot [] ;; <- arglist goes here
-  ;; your code goes here
+(definterface IRobot
+  (getName [])
+  (setName [name])
 )
 
-(defn robot-name [robot] ;; <- arglist goes here
-      ;; your code goes here
+(deftype Robot [^:volatile-mutable name]
+  IRobot
+  (getName [_] name)
+  (setName [this new-name] (set! name new-name ))
 )
 
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
+(def robot-names (list))
+
+(defn random-name []
+  (str (apply str (repeatedly 2 #(rand-nth "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))) (apply str (repeatedly 3 #(rand-nth "0123456789"))))
+)
+
+(defn ->Robot []
+  (let [robot-name (random-name)]
+    ;; (if (some #(= robot-name %) robot-names)
+    ;;   (->Robot)
+    ;;   (
+    ;;     (def robot-names (list* robot-name robot-names))
+        (Robot. robot-name)
+    ;;   )
+    ;; )
+  )
+)
+
+(defn robot []
+  (->Robot)
+)
+
+(defn robot-name [robot]
+  (.getName robot)
+)
+
+(defn reset-name [robot]
+  (.setName robot (random-name))
 )
